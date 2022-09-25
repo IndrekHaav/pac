4d31d83691c7f37f0a972bca0f3d5cc27a01af9e
Indrek Haav <IndrekHaav@users.noreply.github.com>
2022-09-25 18:36:33 +0300

    First version

---
+++ README.md
@@ -0,0 +1,42 @@
+# What is this?
+
+This script - `pac.sh` - is a simple [pacman](https://wiki.archlinux.org/title/Pacman) helper for [Arch Linux](https://archlinux.org/) that provides syntax similar to [apt](https://wiki.debian.org/AptCLI). For example, `pac install <package>` instead of `pacman -S <package>`. It can be useful to those who, like me, sometimes forget the proper pacman flags to use.
+
+The script implements a subset of apt commands and translates them to [corresponding pacman invocations](https://wiki.archlinux.org/title/Pacman/Rosetta).
+
+## What this is not
+
+This script is **not**:
+
+ - a full-featured pacman wrapper
+ - a port of `apt` to Arch
+ - a replacement for AUR helpers like yay
+
+# How to install?
+
+Clone the repo and copy or symlink the script to a directory that's in the $PATH:
+
+```shell
+$ git clone https://github.com/IndrekHaav/pac
+$ ln -s $(realpath pac/pac.sh) ~/.local/bin/pac
+```
+
+If you happen to have a binary called `pac` already installed (check with `which pac`), then just use another name for the symlink.
+
+# How to use?
+
+Run `pac` with no arguments to get an overview of the supported commands:
+
+```shell
+$ pac
+Usage: pac command
+
+Available commands:
+    search <string>         Searches for packages matching <string>
+    show <package>          Returns information about <package>
+    install <package>       Installs <package>
+    remove <package>        Removes <package>
+    autoremove <package>    Removes <package> and all its unneeded dependencies
+    autoremove              Removes all unneeded dependencies
+    upgrade, dist-upgrade   Performs a full system upgrade
+```
---
+++ pac.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+set -eu
+
+RESET="\033[0m"
+RED="\033[1;31m"
+
+__usage() {
+    cat <<EOF
+Usage: $(basename "$0") command
+
+Available commands:
+    search <string>         Searches for packages matching <string>
+    show <package>          Returns information about <package>
+    install <package>       Installs <package>
+    remove <package>        Removes <package>
+    autoremove <package>    Removes <package> and all its unneeded dependencies
+    autoremove              Removes all unneeded dependencies
+    upgrade, dist-upgrade   Performs a full system upgrade
+EOF
+    exit
+}
+
+__error() {
+    echo -e "${RED}error:${RESET} $1"
+}
+
+__fatal() {
+    __error "$1"
+    exit 1
+}
+
+[ "$#" -gt 0 ] || __usage
+
+case "$1" in
+    search)
+        shift
+        [ "$#" -eq 1 ] || __fatal "enter a search term"
+        pacman -Ss "$@"
+        ;;
+    show)
+        shift
+        [ "$#" -gt 0 ] || __fatal "enter a package name"
+        for package in "$@"; do
+            pacman -Si "$package" 2>/dev/null || pacman -Qi "$package" 2>/dev/null || __error "package '$package' was not found"
+        done
+        ;;
+    install)
+        shift
+        [ "$#" -gt 0 ] || __fatal "enter a package name"
+        pacman -S "$@"
+        ;;
+    remove)
+        shift
+        [ "$#" -gt 0 ] || __fatal "enter a package name"
+        pacman -R "$@"
+        ;;
+    autoremove)
+        shift
+        if [ "$#" -eq 0 ]; then
+            readarray -t pkgs < <(pacman -Qdtq)
+            pacman -Rs "${pkgs[@]}"
+        else
+            pacman -Rs "$@"
+        fi
+        ;;
+    update)
+        shift
+        if [ "$#" -eq 0 ]; then
+            __fatal "this command would execute 'pacman -Sy', but partial upgrades are not supported"
+        else
+            __fatal "this command would execute 'pacman -S $*', but partial upgrades are not supported"
+        fi
+        ;;
+    upgrade|dist-upgrade)
+        pacman -Syu
+        ;;
+    *)
+        __usage
+        exit 1
+        ;;
+esac
